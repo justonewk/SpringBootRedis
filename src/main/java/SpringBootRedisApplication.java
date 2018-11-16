bbbf16c0926fef1ea714cefd517ea5891463af1b
wangkun <1207226357@qq.com>
2018-11-16 17:20:56 +0800

    新增RedisUtil类（封装RedisTemplate的RedisUtil类）  RedisConfig类（SpringBoot整合redis）

---
+++ src/main/java/SpringBootRedisApplication.java
@@ -0,0 +1,18 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class SpringBootRedisApplication {
+
+    private static SpringBootRedisApplication instance;
+    public static void main(String args[]){
+        SpringApplication.run(SpringBootRedisApplication.class);
+        instance = (SpringBootRedisApplication) SpringContextUtils.getBeanByClass(SpringBootRedisApplication.class);
+
+    }
+
+    public static SpringBootRedisApplication getInstance() {
+        return instance;
+    }
+}
---
+++ src/main/java/com/wk/config/RedisConfig.java
@@ -0,0 +1,64 @@
+package com.wk.config;
+
+import com.wk.utils.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * SpringBoot整合redis
+ * @author wk
+ */
+@Configuration
+public class RedisConfig {
+    /**
+     * 注入 RedisConnectionFactory
+     */
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+    /**
+     * 实例化 RedisTemplate 对象
+     *
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> functionDomainRedisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        initDomainRedisTemplate(redisTemplate, redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    /**
+     * 设置数据存入 redis 的序列化方式
+     *
+     * @param redisTemplate
+     * @param factory
+     */
+    private void initDomainRedisTemplate(RedisTemplate<String, Object> redisTemplate, RedisConnectionFactory factory) {
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        // 开启事务
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.setConnectionFactory(factory);
+    }
+
+    /**
+     * 注入封装RedisTemplate
+     * @param redisTemplate
+     * @return
+     */
+    @Bean(name = "redisUtil")
+    public RedisUtil redisUtil(RedisTemplate<String, Object> redisTemplate) {
+        RedisUtil redisUtil = new RedisUtil();
+        redisUtil.setRedisTemplate(redisTemplate);
+        return redisUtil;
+    }
+
+}
