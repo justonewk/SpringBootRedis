cda77348b6b20c50d16d05b4f5202a748bc6edfe
wangkun <1207226357@qq.com>
2018-11-19 21:37:33 +0800

    bug处理
    bug处理

--- src/main/java/com/wk/SpringBootRedisApplication.java
+++ src/main/java/com/wk/SpringBootRedisApplication.java
@@ -1,7 +1,6 @@
 package com.wk;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
